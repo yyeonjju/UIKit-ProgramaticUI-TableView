c5d42fd94dfbc5cb6b08eebb4dff372439bd4cba
yyeonjju <llilyha123@gmail.com>
2023-10-28 20:02:27 +0900

    코드로 테이블뷰 구현

---
+++ TableViewCode/Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  TableViewCode
+//
+//  Created by 하연주 on 2023/10/28.
+//
+
+import UIKit
+
+struct Movie {
+    var movieImage: UIImage?
+    let movieName: String
+    let movieDescription: String
+}
